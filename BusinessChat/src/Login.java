d2a22be49629ec94f568fde5ed3ccb97c9e46ff2
htmboy <35950268+htmboy@users.noreply.github.com>
2018-11-02 23:36:25 +0800

    20181102

--- BusinessChat/src/Login.java
+++ BusinessChat/src/Login.java
@@ -2,20 +2,34 @@
 import java.awt.Button;
 import java.awt.FlowLayout;
 import java.awt.Frame;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.TextField;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-public class Login {
-	private Frame f;
-	private Button but;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextArea;
+
+public class Login extends JPanel{
+	private JFrame jf;
+	private GridBagLayout gbl = new GridBagLayout();
+	private JButton but;
+	private JLabel jl_username;
+	private JLabel jl_password;
+	private JTextArea jta;
+	private JPasswordField jpf;
+	
 	public Login() {
-		init();
-	}
-	private void init() {
-		f = new Frame("商务通");
-	//	f.setSize(500, 400);
-	//	f.setLocation(400, 200);
+		super();
+		GridBagConstraints constraints;
+		setLayout(gbl);
+		jl_username = new JLabel("用户名");
+		addComponent(jl_username,0,0,1,1,10,100,GridBagConstraints.NONE, GridBagConstraints.EAST);
 		f.setBounds(400, 200, 500, 400);
 		f.setLayout(new FlowLayout());
 		Button but = new Button("登录");
@@ -31,6 +45,20 @@ public class Login {
 			
 		});
 		f.setVisible(true);
+	}
+
+	private void addComponent(JLabel jl_username2, int gridx, int gridy, int gridwidth, int gridheight, int weightx, int heighty, int fill, int anchor) {
+		GridBagConstraints constraints = new GridBagConstraints();
+		
+		constraints.gridx = gridx;
+		constraints.gridy = gridy;
+		constraints.gridwidth = gridwidth;
+		constraints.gridheight = gridheight;
+		constraints.weightx = weightx;
+		constraints.weighty = heighty;
+		constraints.fill = fill;
+		constraints.anchor = anchor;
+		gridbag.setConstraints(component, constraints);
 	}
 	
 }
